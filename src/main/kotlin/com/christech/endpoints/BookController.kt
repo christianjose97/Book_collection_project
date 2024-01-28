47b8604044a2a3114da3d64faea28df189940a27
Chris <christian.jose443@gmail.com>
2024-01-28 17:31:01 -0500

    add put functionality

--- src/main/kotlin/com/christech/endpoints/BookController.kt
+++ src/main/kotlin/com/christech/endpoints/BookController.kt
@@ -4,11 +4,7 @@ import com.christech.books.Book
 import com.christech.books.request.BookRequest
 import com.christech.service.BookService
 import io.micronaut.http.HttpStatus
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Status
+import io.micronaut.http.annotation.*
 import jakarta.validation.Valid
 
 @Controller("/book")
@@ -27,9 +23,20 @@ class BookController(
     fun getAll() =
             bookService.getAll()
 
+    @Put("/{id}")
+    fun update (
+                id: String, @Body @Valid
+            request: BookRequest,
+            @Header("X-test") header: String
+    ): Book {
+        println("Header: $header")
+
+        return bookService.update(id, request.toModel())
+    }
+
     @Get("/{id}")
     fun getById(id: String) =
-            bookService.getById()
+            bookService.getById(id)
     private fun BookRequest.toModel() : Book =
             Book(
                     title = this.title,
--- src/main/kotlin/com/christech/service/BookService.kt
+++ src/main/kotlin/com/christech/service/BookService.kt
@@ -5,6 +5,7 @@ import com.christech.repository.BookRepository
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.exceptions.HttpStatusException
 import jakarta.inject.Singleton
+import org.bson.types.ObjectId
 
 @Singleton
 class BookService (
@@ -18,8 +19,21 @@ class BookService (
             bookRepository.findAll()
                     .toList()
 
-    fun getById(id: String): Book
-    = bookRepository.findById(id)
-            .orElseThrow { HttpStatusException(HttpStatus.NOT_FOUND, "User with id $id was not found.") }
+    fun getById(id: String): Book {
+        if (!ObjectId.isValid(id)) {
+            throw HttpStatusException(HttpStatus.BAD_REQUEST, "Invalid ID format: $id")
+        }
+
+        return bookRepository.findById(id)
+                .orElseThrow { HttpStatusException(HttpStatus.NOT_FOUND, "Book with ID $id was not found.") }
+    }
+
+    fun update(id: String, book: Book): Book {
+        val found = getById(id)
+
+        val updated = book.copy(id = found.id)
+
+        return bookRepository.update(updated)
+    }
 
 }
